426762fffed152573674471179840f636db7f63f
Jacquiiii <jacqui.koroll@telus.com>
2023-03-19 22:32:22 -0700

    feat: add userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,8 @@
-//External Imports
+// External Imports
 const express = require('express')
-const router  = express.Router()
+const router = express.Router()
 
-// Internal Helper function
+// Helper function
 const db = require('../db/connection')
 
 // Get list of all users
@@ -10,16 +10,35 @@ router.get('/', (req, res) => {
   const userQuery = `SELECT * FROM users;`
 
   db.query(userQuery)
-  .then(data => {
-    const userData = data.rows;
-    res.json({userData})
-  })
-  .catch( err => {
-    res
-      .status(500)
-      .json({error: err.message})
-  })
+    .then(data => {
+      const userData = data.rows;
+      res.json({ userData })
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message })
+    })
+})
 
+// Get user by email
+router.get('/:email', (req, res) => {
+
+  const userEmailQuery = `
+    SELECT * FROM users 
+    WHERE email = ${req.params.id};;
+  `
+
+  db.query(userEmailQuery)
+    .then(data => {
+      const userData = data.rows;
+      res.json({ userData })
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message })
+    })
 })
 
 module.exports = router
